af1c0acf81c29855b250216060a51b8aaf209770
76do <isshi0210@gmail.com>
2023-04-09 14:13:15 +0900

    Add: 商品の新規登録を行うmutationを作成した。

--- backend/app/graphql/mutations/create_item.rb
+++ backend/app/graphql/mutations/create_item.rb
@@ -1,5 +1,25 @@
 module Mutations
   class CreateItem < BaseMutation
     field :item, Types::ItemType, null: false
+
+    argument :shop_id, Integer, required: true
+    argument :item_attributes, Types::ItemAttributes, required: true
+
+    def resolve(shop_id:, item_attributes:)
+      puts shop_id
+      puts item_attributes
+      shop = Shop.find(shop_id)
+      unless shop
+        rails GraphQL::ExecutionError, "Shop not found"
+      end
+      item = shop.items.build(item_attributes.to_h)
+      if item.save
+        {
+          item: item
+        }
+      else
+        raise GraphQL::ExecutionError, item.errors.full_messages
+      end
+    end
   end
 end
