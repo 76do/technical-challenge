3f8734df3c56321d1824c2de7085c3af37ac789c
76do <isshi0210@gmail.com>
2023-04-09 15:20:11 +0900

    Add: itemモデルにaccepts_nested_attributes_for :variations,allow_destroy: trueを追加した。
    _destroy属性を使用できるようにするために、accepts_nested_attributes_for :variations,allow_destroy: true
    を追記した。

--- backend/app/models/item.rb
+++ backend/app/models/item.rb
@@ -23,7 +23,7 @@ class Item < ApplicationRecord
   enum status: { unpublished: 0, published: 1 }
   belongs_to :shop
   has_many :variations, dependent: :destroy
-  accepts_nested_attributes_for :variations
+  accepts_nested_attributes_for :variations, allow_destroy: true
 
   validates :price, presence: true
   validates :status, presence: true
